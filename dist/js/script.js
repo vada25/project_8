b3ddfc981fd956835d554dd0cdb3af31c1d6282c
vada25 <vada1981@gmail.com>
2024-06-19 23:06:18 +0300

    upd_2

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
   $('.carousel__inner').slick({
-    infinite: true,
-    slideToShow: 1,
-    slideToScroll: 3
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      dots: true,
+      arrows: false,
+      infinite: true,
   });
 });
