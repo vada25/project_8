c5573d50e3deb567b06a2d957ca48d9c40cde1cb
vada25 <vada1981@gmail.com>
2024-06-20 17:10:09 +0300

    upd_3

--- src/js/script.js
+++ src/js/script.js
@@ -8,4 +8,15 @@ $(document).ready(function(){
       arrows: false,
       infinite: true,
   });
+  $('.carousel__bottom').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: false,
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+    infinite: true,
+});
 });
